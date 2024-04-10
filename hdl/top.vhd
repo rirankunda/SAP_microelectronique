449a6776aa4777e6453618f795af9f8042c686b4
1tnx <73384301+1tnx@users.noreply.github.com>
2024-04-10 11:26:49 +0200

    project restructuration

---
+++ hdl/top.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity top is
+    port (
+        clk   : in std_logic
+    );
+end entity;
+
+architecture behavourial of top is
+
+begin
+
+end architecture;
---
+++ tb/tb_top.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library vunit_lib;
+context vunit_lib.vunit_context;
+
+entity tb_top is
+  generic (runner_cfg : string);
+end entity;
+
+architecture tb of tb_top is
+begin
+
+end architecture;
